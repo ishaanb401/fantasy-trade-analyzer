d6181745468f2750d47a4efda17059fd31b1b067
Ganesh Saranga <ganeshsaranga@Ganeshs-MacBook-Pro-2.local>
2023-10-15 00:38:01 -0500

    Uncommented line in order to resolve conflicts

--- Fantasy Football Trade Analyzer/src/Components/SearchBar.jsx
+++ Fantasy Football Trade Analyzer/src/Components/SearchBar.jsx
@@ -7,19 +7,19 @@ export const SearchBar = ({ setResults }) => {
   const [input, setInput] = useState("");
 
   const fetchData = (value) => {
-    fetch("https://sports.core.api.espn.com/v2/sports/football/leagues/nfl/athletes?limit=1000&active=true")
+    fetch("")//https://sports.core.api.espn.com/v2/sports/football/leagues/nfl/athletes?limit=1000&active=true")
       .then((response) => response.json())
       .then((json) => {
         console.log(json); //
-        // const results = json.filter((user) => {
-        //   return (
-        //     value &&
-        //     user &&
-        //     user.fullName &&
-        //     user.fullName.toLowerCase().includes(value)
-        //   );
-        // });
-        //  setResults(results);
+        const results = json.filter((user) => {
+          return (
+            value &&
+            user &&
+            user.fullName &&
+            user.fullName.toLowerCase().includes(value)
+          );
+        });
+         setResults(results);
       });
   };
 

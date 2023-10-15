21d10eff85e4d338af29d59ce8dc1d27fef4875b
Ganesh Saranga <ganeshsaranga@Ganeshs-MacBook-Pro-2.local>
2023-10-15 00:17:35 -0500

    established API connection, changed UI

--- app.js
+++
@@ -1,7 +0,0 @@
-import { Client } from 'espn-fantasy-football-api';
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-// Create a Client instance and configure it
-const myClient = new Client({ leagueId: 387659 });
-myClient.setCookies({ espnS2: 'YOUR_ESPN_S2', SWID: 'YOUR_SWID' });
